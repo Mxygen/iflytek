5c561bbc40b88123013d5f69e765d62bc0dfc43c
泠玖 <202205100111@zjut.edu.cn>
2025-07-14 19:17:35 +0800

    直角特殊处理

--- src/visual_navigation/include/detect_edge.h
+++ src/visual_navigation/include/detect_edge.h
@@ -8,4 +8,5 @@ void Detect_Edge(cv::Mat &original_frame, cv::Mat &detect_edge_frame, double low
 void Canny_Crawl_L_R(const std::vector<std::vector<uint8_t>> &image, uint8_t width, uint8_t height, uint8_t highest);
 bool Canny_Crawl_Top(const std::vector<std::vector<uint8_t>> &image, uint8_t left, uint8_t right);
 bool Canny_Method(cv::Mat &original_frame, double lowthreshold, double highthreshold,int Trace_edge, bool Debug);
+bool RightDetect(const std::vector<std::vector<uint8_t>> &image, uint8_t left, uint8_t right,int &Dir);
 #endif
--- src/visual_navigation/src/detect_edge.cpp
+++ src/visual_navigation/src/detect_edge.cpp
@@ -1,7 +1,8 @@
 #include "../include/detect_edge.h"
 
 using namespace cv;
-#define road_width 130
+using namespace std;
+#define road_width 120
 uint8_t current_border_L[74] = {0};
 uint8_t current_border_R[74] = {0};
 uint8_t last_border_L[74] = {0};
@@ -18,6 +19,8 @@ bool Canny_Method(Mat &original_frame, double lowthreshold, double highthreshold
     cvtColor(original_frame, original_frame, COLOR_BGR2GRAY);                   // 原始图像转换灰度图像
     // GaussianBlur(original_frame, original_frame, Size(5, 5), 1.5);              // 高斯滤波
     Canny(original_frame, original_frame, lowthreshold, highthreshold);         // Canny算子边缘检测
+    bool RightAngle = false;
+    int Dir = 0;
     // cv::imshow("original_frame",original_frame);
     // cv::waitKey(1);
     /*中心提取*/
@@ -36,7 +39,7 @@ bool Canny_Method(Mat &original_frame, double lowthreshold, double highthreshold
         if (Trace_edge == 9)
         {
 
-            if (Canny_Crawl_Top(image, original_frame.cols * 0.4, original_frame.cols * 1.0))
+            if (Canny_Crawl_Top(image, original_frame.cols * 0.2, original_frame.cols * 0.8))
             {
                 ROS_INFO("Quit");
                 return true;
@@ -48,7 +51,11 @@ bool Canny_Method(Mat &original_frame, double lowthreshold, double highthreshold
         }
         /*爬取边线*/
         Canny_Crawl_L_R(image, original_frame.cols, original_frame.rows, original_frame.rows * 0.6);
-
+        if(RightDetect(image, original_frame.cols * 0.2, original_frame.cols * 0.8, Dir))
+        {
+            ROS_INFO("Right Detect");
+            RightAngle = true;
+        }
         /*计算中线*/
         for (int i = original_frame.rows * 0.6; i < original_frame.rows - 1; i++)
         {
@@ -64,6 +71,13 @@ bool Canny_Method(Mat &original_frame, double lowthreshold, double highthreshold
             else
             {
                 G_line_M[i] = (G_border_L[i] + G_border_R[i]) / 2; // 简单取均值
+                if (RightAngle)
+                {
+                    if (Dir == 1)
+                        G_line_M[i] -= road_width / 4;
+                    else if (Dir == 2)
+                        G_line_M[i] += road_width / 4;   
+                }
             }
 
             if (Debug)
@@ -153,8 +167,6 @@ bool Canny_Crawl_Top(const std::vector<std::vector<uint8_t>> &image, uint8_t lef
     }
     average /= average_count;
     // ROS_INFO("average_count: %d", average_count);
-    if (average_count > 95)
-        ROS_INFO("average: %d", average);
 
     if (average_count > 0.8 * (right - left) && average > 119 - thres)
     {
@@ -165,4 +177,75 @@ bool Canny_Crawl_Top(const std::vector<std::vector<uint8_t>> &image, uint8_t lef
     {
         return false;
     }
+}
+
+bool RightDetect(const std::vector<std::vector<uint8_t>> &image, uint8_t left, uint8_t right,int &Dir)
+{
+
+    uint8_t count = 0;
+    int direction = 0;
+    for(int i = 72; i < 110; i++)
+    {
+        if(G_border_R[i] == 159 ^ G_border_L[i] == 0)
+        {
+            count ++;
+            if (G_border_L[i] == 0)
+                direction ++;
+            else 
+                direction --;
+        }
+    }
+    if (count < 15)
+        return false;
+    else if (direction >= 0)
+        Dir = 1;
+    else if (direction < 0)
+        Dir = 2;
+
+
+
+    count = 0;
+    uint8_t tmp[right - left] = {0};
+    uint8_t height = 72;
+    uint8_t thres = 18;
+    for (int i = left; i < right; i++)
+    {
+        for (int j = 119; j > 72; j--)
+        {
+            if (image[j][i])
+            {
+                count++;
+                tmp[i - left] = j;
+                break;
+            }
+            
+        }
+    }
+    int average = 0, average_count = 0;
+    //方差
+    double variance = 0.0;
+    for (int i = 1; i < right - left - 2; i++)
+    {
+        if (abs(tmp[i] - tmp[i - 1]) < 2)
+        {
+            average += tmp[i];
+            average_count++;
+        }
+    }
+    average /= average_count;
+    for (int i = 1; i < right - left - 2; i++)
+    {
+        variance += (tmp[i] - average) * (tmp[i] - average);
+    }
+    variance /= average_count;
+    ROS_WARN("variance: %f", variance);
+
+    // ROS_INFO("average_count: %d", average_count);
+
+    if (average_count > 0.6 * (right - left) and average < 84 and variance < 10)
+    {
+        return true;
+    }
+    else
+        return false;
 }
